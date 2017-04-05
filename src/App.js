cc193739509ec89b552dad986c2531b4dae7a98a
thelittleblacksmith <einers@outlook.com>
2017-04-04 21:26:31 -0700

    nested routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <nav>
+          <p><Link to="/">Home</Link></p>
+          <p><Link to="/about">About</Link></p>
+          <p><Link to="/repos">Repos</Link></p>
+        </nav>
+        <h1>Home page</h1>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { Router, Route, hashHistory } from 'react-router';
+
+//module imports
+import About from './modules/About';
+import Repos from './modules/Repos';
 
 ReactDOM.render(
-  <App />,
+  <Router history={hashHistory}>
+    {/*add routes here*/}
+    <Route path="/" component={App}>
+      <Route path="/about" component={About}/>
+      <Route path="/repos" component={Repos}/>
+    </Route>    
+  </Router>,
   document.getElementById('root')
 );
+
