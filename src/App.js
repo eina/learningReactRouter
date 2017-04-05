d281a8d0c87d8130be6e63df96f712684a347177
thelittleblacksmith <einers@outlook.com>
2017-04-04 22:09:02 -0700

    IndexLink + onlyActiveOnIndex

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router';
+import { Link, IndexLink } from 'react-router';
 import './App.css';
 
 import NavLink from './modules/NavLink';
@@ -9,7 +9,11 @@ class App extends Component {
     return (
       <div className="App">
         <nav>
-          <p><NavLink to="/">Home</NavLink></p>
+          {/*parent routes are active when child routes are active*/}
+          {/* 'onlyActiveOnIndex={true}' would work if passed to NavLink as well*/}
+          {/*<p><NavLink to="/">Home</NavLink></p>*/}
+          {/*use IndexLink to only select index*/}
+          <p><IndexLink to="/" activeClassName="active">Home</IndexLink></p>
           <p><NavLink to="/about"  activeClassName="active">About</NavLink></p>
           <p><NavLink to="/repos" activeClassName="active">Repos</NavLink></p>
         </nav>
